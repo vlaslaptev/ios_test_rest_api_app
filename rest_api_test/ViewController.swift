fcd939c0bc905b629752c9a56b7dfd076b4dabe2
v.laptev <v.laptev@qiwi.com>
2018-03-22 23:36:23 +0300

    Initial Commit

---
+++ rest_api_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rest_api_test
+//
+//  Created by v.laptev on 22.03.2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
