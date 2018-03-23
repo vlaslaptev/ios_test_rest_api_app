7f18b3a9103e0f1155f6cfb5a38fcadf8b819f1d
v.laptev <v.laptev@qiwi.com>
2018-03-23 22:23:18 +0300

    test son parse

--- rest_api_test/ViewController.swift
+++ rest_api_test/ViewController.swift
@@ -15,23 +15,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var btcPrice: UILabel!
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet weak var labelTest: UILabel!
     
     @IBAction func tapBtn(_ sender: Any) {
-        guard var url = URL(string: "https://whatthecommit.com") else { return }
+        guard var url =
+            URL(string: "https://api.coinmarketcap.com/v1/ticker/?limit=1")
+            else { return }
         
         let session = URLSession.shared
         session.dataTask(with: url) { (data, response, error) in
             if let response = response {
-                self.labelTest.text = "ok"
-                print(response)
+                print(response);
             }
-            }.resume()
+            guard let data = data else {return}
+            print(data)
+            do {
+                let json = try? JSONSerialization.jsonObject(with: data, options: [])
+                if let root = json as? [Any] {
+                    if let dictionary = root.first as? [String: Any] {
+                        self.btcPrice.text = dictionary["price_usd"] as! String
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
 }
-
